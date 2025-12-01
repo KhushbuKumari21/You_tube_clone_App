bb4cb199665d83214438285d4001252aef016b57
khushbu <pujakumari29122001@gmail.com>
2025-12-02 00:03:26 +0530

    Add PageNotFound component with comments

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -1,3 +1,7 @@
+// src/pages/PageNotFound.jsx
+// This component displays a simple "Page Not Found" message with an image 
+// and a link to navigate back to the home page.
+
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/pageNotFound.css";
@@ -6,12 +10,14 @@ const PageNotFound = () => {
   return (
     <div className="pnf-container">
       <div className="pnf-wrapper">
+        {/* Image illustrating page not found */}
         <img
           className="pnf-image"
           src="https://cdn1.iconfinder.com/data/icons/photo-stickers-words/128/word_18-1024.png"
           alt="Page Not Found"
         />
 
+        {/* Link to redirect user back to home page */}
         <Link to="/" className="pnf-title">
           Go to Home
         </Link>
